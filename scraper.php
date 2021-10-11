127b0aa2c504850853e55061933bcb2c7ffcb926
Ryan Williams <ryanwilliamsdesktop@outlook.com>
2021-10-11 14:39:03 +0100

    comments

--- scraper.php
+++ scraper.php
@@ -7,9 +7,10 @@ $client = new \Goutte\Client();
 
 $crawler = $client->request('GET', 'https://videx.comesconnected.com/');
 
-
+//use xpath to get relevant section of webpage
 $links = $crawler->evaluate('//*[@id="maincontent"]/div[2]/div[1]/div[2]');
 
+//TODO parse string to extract data
 foreach ($links as $link) {
     echo preg_replace('/\s+/', '', $link->textContent.PHP_EOL);
 }
